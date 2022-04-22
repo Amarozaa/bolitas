37e95dcf23d18501c0b80dec7c574e9ff0cfa543
Amarozaa <102813947+Amarozaa@users.noreply.github.com>
2022-04-22 11:03:54 -0400

    22-04 Probando cambio
    22 04 probando cambio

--- charge.gd
+++ charge.gd
@@ -1,20 +1,15 @@
 extends RigidBody2D
-var contador = 0
 
 func power1(direction,distance):
 	print(direction)
-	print("poder1")
 	apply_central_impulse(direction*500/distance)
 	pass
 
 func power2(r_direction,distance,direction):
-	contador = contador + 1
-	print('el contador es '+ str(contador))
-	print(distance)
-	#print('hola soy d '+ "d",direction)
-	#print('hola soy r '+"r",r_direction)
-	apply_central_impulse(6*r_direction)
 	
+	print("d",direction)
+	print("r",r_direction)
+	apply_central_impulse(10*r_direction)
 	pass
 # Called when the node enters the scene tree for the first time.
 
--- kinematic.gd
+++ kinematic.gd
@@ -10,6 +10,10 @@ onready var anim_tree=$AnimationTree
 onready var playback = anim_tree.get("parameters/playback")
 export var positive= true
 onready var charge=get_parent().get_node("charge")
+onready var joint = get_node("PinJoint2D")
+onready var mono = get_parent().get_node("character")
+
+
 
 
 #activar tree
@@ -19,7 +23,9 @@ func _ready():
 func _physics_process(delta):
 	match state:
 		State.MOVING:
+			joint.set_node_b(mono.get_path())
 			moving_physics_process(delta)
+			
 		State.POWER_1:
 			power1_physics_process(delta)
 		State.POWER_2:
@@ -49,10 +55,11 @@ func moving_physics_process(delta):
 			
 	if Input.is_action_just_pressed("power_1"):
 		state=State.POWER_1
-		
 		return
 		
 	if Input.is_action_just_pressed("power_2"):
+		
+		joint.set_node_b(charge.get_path())
 		state=State.POWER_2
 		return
 		
@@ -82,17 +89,30 @@ func power1_physics_process(_delta):
 	var direction=global_position.direction_to(charge.global_position)
 	var distance=global_position.distance_to(charge.global_position)
 	charge.power1(direction,distance)
+	vel=move_and_slide(vel,Vector2(0,-1),  false,   4,   0.785398, true)
+	vel.y+=grav	
+	vel.x=0
+	
 	
 	if Input.is_action_just_released("power_1"):
 		state=State.MOVING
 		return
 	
 	
+	
 func power2_physics_process(_delta):
-	var direction=global_position.direction_to(charge.global_position) #vector unitario que apunta del pj a la carga
-	var r_direction=Vector2(direction[1],-direction[0])  #
+	playback.travel("ani_power_1")
+	var direction=global_position.direction_to(charge.global_position)
+	var r_direction=Vector2(direction[1],-direction[0])
 	var distance=global_position.distance_to(charge.global_position)
 	charge.power2(r_direction,distance,direction)
+	
+	
+	vel=move_and_slide(vel,Vector2(0,-1),  false,   4,   0.785398, true)
+	vel.y+=grav	
+	vel.x=0
+	
+	
 	
 	if Input.is_action_just_released("power_2"):
 		state=State.MOVING
