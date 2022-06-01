5eeed6c83660ed81cab68fc907463ecef934665c
Amarozaa <102813947+Amarozaa@users.noreply.github.com>
2022-06-01 11:46:18 -0400

    Version 1 junio amaro z
    Version 1 junio amaro z

--- charge.gd
+++ charge.gd
@@ -1,13 +1,17 @@
 extends RigidBody2D
 
-func power1(direction,distance):
+func power1(direction,distance):   
+	
+	 
 	print(direction)
 	apply_central_impulse(direction*500/distance)
 	pass
-
-func power2(r_direction,distance,direction):
+		
 	
-	print("d",direction)
+
+func power2(r_direction):
+	
+
 	print("r",r_direction)
 	apply_central_impulse(10*r_direction)
 	pass
--- kinematic.gd
+++ kinematic.gd
@@ -17,6 +17,19 @@ onready var mono = get_parent().get_node("character")
 onready var sprite = $sprite
 
 
+
+
+
+#consts
+
+const UP = "up_{num}"
+const LEFT = "left_{num}"
+const RIGHT = "right_{num}"
+const POWER_1 = "power_1_{num}"
+const POWER_2 = "power_2_{num}"
+
+onready var player_index = 1
+
 #activar tree
 func _ready():
 	if Engine.editor_hint:
@@ -24,6 +37,11 @@ func _ready():
 		return
 		
 	anim_tree.active = true
+	player_index = 1 if positive else 2
+	
+	self.positive = positive
+
+
 
 
 func _physics_process(delta):
@@ -41,15 +59,15 @@ func _physics_process(delta):
 			power2_physics_process(delta)
 	
 	
-	if Input.is_action_just_pressed("test"):
-		self.positive = !positive
+	
 			
 			
+	
 			
 func moving_physics_process(delta):
 	
 	#movimientos horizontales
-	var oriented=Input.get_axis("left","right")
+	var oriented=Input.get_axis(LEFT.format({"num":player_index}),RIGHT.format({"num":player_index}))
 	vel.x=lerp(vel.x,x_vel*oriented,0.3)
 	vel=move_and_slide(vel,Vector2(0,-1),  false,   4,   0.785398, false)
 
@@ -57,25 +75,24 @@ func moving_physics_process(delta):
 	# saltos
 	if is_on_floor():
 		c_jump=0
-		if Input.is_action_just_pressed("up") :
+		if Input.is_action_just_pressed(UP.format({"num":player_index})) :
 			vel.y=jump_vel
 			c_jump+=1		
 	else:
-		if Input.is_action_just_pressed("up")and c_jump<=1:
+		if Input.is_action_just_pressed(UP.format({"num":player_index}))and c_jump<=1:
 			vel.y=jump_vel
 			c_jump+=1
 			
 			
-	if Input.is_action_just_pressed("power_1"):
+	if Input.is_action_just_pressed(POWER_1.format({"num":player_index})):
 		state=State.POWER_1
 		return
 		
-	if Input.is_action_just_pressed("power_2"):
+	if Input.is_action_just_pressed(POWER_2.format({"num":player_index})):
 		
 		joint.set_node_b(charge.get_path())
 		state=State.POWER_2
 		return
-		
 		
 	#Animation
 	if is_on_floor():
@@ -88,18 +105,19 @@ func moving_physics_process(delta):
 				get_node('sprite').set_flip_h( true )
 		else:	
 			playback.travel('Idle')	
-		if Input.is_action_just_pressed("up"):
+		if Input.is_action_just_pressed(UP.format({"num":player_index})):
 			playback.travel('jump')	
 	else:
-		if Input.is_action_just_pressed("up") and c_jump!=1:
+		if Input.is_action_just_pressed(UP.format({"num":player_index})) and c_jump!=1:
 			playback.travel('double_jump')
-		if Input.is_action_just_pressed("up") and c_jump==0:
+		if Input.is_action_just_pressed(UP.format({"num":player_index})) and c_jump==0:
 			playback.travel('jump')
 	
 	
 func power1_physics_process(_delta):
 	playback.travel("ani_power_1")
 	var direction=global_position.direction_to(charge.global_position)
+	direction = direction if positive else -direction
 	var distance=global_position.distance_to(charge.global_position)
 	charge.power1(direction,distance)
 	vel=move_and_slide(vel,Vector2(0,-1),  false,   4,   0.785398, true)
@@ -107,7 +125,7 @@ func power1_physics_process(_delta):
 	vel.x=0
 	
 	
-	if Input.is_action_just_released("power_1"):
+	if Input.is_action_just_released(POWER_1.format({"num":player_index})):
 		state=State.MOVING
 		return
 	
@@ -116,9 +134,9 @@ func power1_physics_process(_delta):
 func power2_physics_process(_delta):
 	playback.travel("ani_power_1")
 	var direction=global_position.direction_to(charge.global_position)
+	direction = direction if positive else -direction 
 	var r_direction=Vector2(direction[1],-direction[0])
-	var distance=global_position.distance_to(charge.global_position)
-	charge.power2(r_direction,distance,direction)
+	charge.power2(r_direction)
 	
 	
 	vel=move_and_slide(vel,Vector2(0,-1),  false,   4,   0.785398, true)
@@ -127,7 +145,7 @@ func power2_physics_process(_delta):
 	
 	
 	
-	if Input.is_action_just_released("power_2"):
+	if Input.is_action_just_released(POWER_2.format({"num":player_index})):
 		state=State.MOVING
 		return
 
@@ -138,8 +156,13 @@ func update_region():
 
 func set_positive(value):
 	positive = value
-	if positive:
-		sprite.region_rect.position = Vector2.ZERO
-	else:
-		sprite.region_rect.position = Vector2(0, 32)
-	update_region()
+	if sprite:
+		
+		if positive:
+			sprite.region_rect.position = Vector2.ZERO
+		else:
+			sprite.region_rect.position = Vector2(0, 32)
+		update_region()
+	
+
+	
